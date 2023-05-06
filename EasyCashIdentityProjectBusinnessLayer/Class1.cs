5c4d5a1b75d7d15e820df8c6b687d5252e748828
Murat Parça <selfdesigner@hotmail.com>
2023-05-06 23:45:25 +0300

    concreate sınıf işlemleri

---
+++ EasyCashIdentityProjectBusinnessLayer/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace EasyCashIdentityProjectBusinnessLayer
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ EasyCashIdentityProjectEntityLayer/Concreate/CustomerAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProjectEntityLayer.Concreate
+{
+    public class CustomerAccount
+    {
+        public int CustomerAccountID { get; set; }
+        public int CustomerAccountNumber { get; set; }
+        public int CustomerAccountCurrency { get; set;}
+        public int CustomerAccountBalance { get; set; }
+        public int BankBranch { get; set; }
+    }
+}
